16aa9404377825ba9df249c873e2d5b718958c6c
Georgi Mutafov <145926316+gmutafov@users.noreply.github.com>
2024-06-20 20:59:00 +0300

    Exercises

--- Python_ORM/Django Models Basics - Exercise/blog.py
+++
@@ -1,6 +0,0 @@
-from django.db import models
-
-
-class Blog(models.Model):
-    post = models.TextField()
-    author = models.CharField(max_length=35)
--- Python_ORM/Django Models Basics - Exercise/weather_forecast.py
+++
@@ -1,7 +0,0 @@
-from django.db import models
-
-class WeatherForecast(models.Model):
-    date = models.DateField()
-    temperature = models.FloatField()
-    humidity = models.FloatField()
-    precipitation = models.FloatField()
