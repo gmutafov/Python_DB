d3d064d67fe036b890121828a39ee3cda13e8e42
Georgi Mutafov <145926316+gmutafov@users.noreply.github.com>
2024-07-10 19:23:37 +0300

    Exercise

---
+++ Python_ORM/Advanced Django Model Techniques - Lab/caller.py
@@ -0,0 +1,40 @@
+import os
+import django
+from django.core.exceptions import ValidationError
+
+# Set up Django
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "orm_skeleton.settings")
+django.setup()
+
+# Import your models here
+
+# Create queries within functions
+from main_app.models import Restaurant, Menu
+
+# Keep the data from the previous exercise, so you can reuse it
+
+valid_menu = Menu(
+    name="Menu at The Delicious Bistro",
+    description="** Appetizers: **\nSpinach and Artichoke Dip\n** Main Course: **\nGrilled Salmon\n** Desserts: **\nChocolate Fondue",
+    restaurant=Restaurant.objects.first(),
+)
+
+try:
+    valid_menu.full_clean()
+    valid_menu.save()
+    print("Valid Menu saved successfully!")
+except ValidationError as e:
+    print(f"Validation Error: {e}")
+
+invalid_menu = Menu(
+    name="Incomplete Menu",
+    description="** Appetizers: **\nSpinach and Artichoke Dip",
+    restaurant=Restaurant.objects.first(),
+)
+
+try:
+    invalid_menu.full_clean()
+    invalid_menu.save()
+    print("Invalid Menu saved successfully!")
+except ValidationError as e:
+    print(f"Validation Error: {e}")
